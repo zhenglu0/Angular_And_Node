b7eb48fcb66b4e59731b8a3823b9704b0912c7fd
Zheng Luo <luozheng03@gmail.com>
2016-04-16 19:37:13 -0400

    Move files from Others to outer directory

---
+++ Node/Node_Beginner/requestHandlers.js
@@ -0,0 +1,66 @@
+var querystring = require("querystring"),
+    formidable = require("formidable"),
+    fs = require("fs"),
+    util = require('util');
+
+function start(request, response) {
+  console.log("Request handler 'start' was called.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(request, response) {
+  console.log("Request handler 'upload' was called.");
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+    /* Possible error on Windows systems:
+    tried to rename to an already existing file */
+    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+      if (err) {
+        fs.unlink("/tmp/test.png");
+        fs.rename(files.upload.path, "/tmp/test.png");
+      }
+    });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+    console.log(util.inspect({fields: fields, files: files}));
+  });
+}
+
+function show(request, response) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
---
+++ Node/Others/helloworld.js
@@ -0,0 +1,7 @@
+//console.log("Hello World");
+
+function execute(someFunction, value) {
+someFunction(value);
+}
+
+execute(function(word){ console.log(word) }, "Hello");
